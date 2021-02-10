6a85232914665f7cd670b23fac61dbcc76e0216e
Baitemir <ferrari170604@gmail.com>
2021-02-10 21:15:58 +0600

    setted questions

--- index.js
+++ index.js
@@ -18,128 +18,132 @@ teams.addEventListener('mousedown', function(event){
 
 let questions = document.querySelector('.questions');
 let card = document.querySelector('.card');
+let question = document.querySelector('.question');
+function questionWrite(qid){
+    question.innerText = qid;
+}
 
 questions.addEventListener('mousedown', function(event){
     if(event.target.tagName == 'DIV'){
         if(event.target.getAttribute('id') == 'n1q1'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n1q2'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n1q3'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n1q4'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n1q5'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n2q1'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n2q2'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n2q3'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n2q4'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n2q5'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n3q1'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n3q2'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n3q3'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n3q4'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n3q5'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n4q1'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n4q2'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n4q3'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n4q4'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n4q5'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n5q1'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n5q2'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n5q3'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n5q4'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n5q5'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n6q1'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n6q2'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n6q3'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n6q4'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n6q5'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n7q1'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n7q2'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n7q3'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n7q4'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n7q5'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n8q1'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n8q2'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n8q3'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n8q4'){
-            
+            questionWrite("");
         }
         if(event.target.getAttribute('id') == 'n8q5'){
-            
+            questionWrite("");
         }
         
     }
